da0f1a86c4a40c5442488cdc87506e5f53babcb7
Nick Rempel <nicholas.rempel@servmetrics.com>
2017-02-26 18:56:18 -0800

    add some tests

--- tmp.js
+++ tmp.js
@@ -4,7 +4,7 @@ const cron = new SimpleCron();
 
 cron.run();
 
-const abc = cron.schedule('* * * * *', () => { console.log(1); });
+// const abc = cron.schedule('* * * * *', () => { console.log(1); });
 
 setTimeout(() => {
   cron.schedule('* * * * *', () => { console.log(2); });
